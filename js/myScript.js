a6ba28b7960b297a8ac5c520167ba3f2e020906f
errollw <errollw@gmail.com>
2014-01-10 15:26:23 +0000

    Smoothing resets between each stroke

--- js/myScript.js
+++ js/myScript.js
@@ -3,37 +3,55 @@ tool.maxDistance = 100;
 
 var path;
 
+var SPEED_MIN = 5,
+	SPEED_MAX = 10;
 var speed_hist = [0, 0, 0, 0, 0];
 
+var first_drag = true;
+
+function speed_to_thickness(speed){
+
+	speed_hist.shift();
+	speed_hist.push(speed);
+
+	var sum = 0;
+	for(var i = 0; i < speed_hist.length; i++)
+	    sum += parseInt(speed_hist[i]);
+	
+	return 12-(sum/speed_hist.length);
+}
+
 function onMouseDown(event) {
+	// start drawing by making a new path
+
 	path = new Path();
 	path.fillColor = '#00000';
-	//path.selected = true;
-
 	path.add(event.point);
 }
 
 function onMouseDrag(event) {
 
-	var s = event.delta.length
-	s = Math.max(6, s);
-	s = Math.min(10, s)
-	speed_hist.shift();
-	speed_hist.push(s);
-
-	var sum = 0;
-	for(var i = 0; i < speed_hist.length; i++){
-	    sum += parseInt(speed_hist[i]);
+	// get the clamped speed that the user is drawing at
+	var speed = event.delta.length;
+	speed = Math.min(SPEED_MAX, Math.max(SPEED_MIN, speed));
+	
+	// reset smoothing speed window at the beginning of a stroke
+	if (first_drag){
+		for(var i = 0; i < speed_hist.length; i++)
+		    speed_hist[i] = speed;
+		first_drag = false;
 	}
 
-	var avg = sum/speed_hist.length;
+	// get thickness to draw at that point
+	var thickness = speed_to_thickness(speed);
 
-	var step = event.delta.normalize(12-avg);
+	// make orthogonal vector to simulate brush thickness
+	var step = event.delta.normalize(thickness);
 	step.angle += 90;
 
+	// add two points to either side of the drawn point
 	var top = event.middlePoint + step;
 	var bottom = event.middlePoint - step;
-
 	path.add(top);
 	path.insert(0, bottom);
 
@@ -45,10 +63,6 @@ function onMouseUp(event) {
 	path.closed = true;
 	
 	path.smooth();
-	//path.simplify();
-}
-
-Array.prototype.rotate = function( n ) {
-  this.unshift.apply( this, this.splice( n, this.length ) )
-  return this;
+	
+	first_drag = true;
 }
