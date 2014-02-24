c92386112d2c6591a0ee1e8040c15ce4b343c896
errollw <errollw@gmail.com>
2014-02-24 14:37:44 +0000

    Split up multitouch example

--- multitouch.js
+++ multitouch.js
@@ -29,6 +29,27 @@ window.onload = function() {
     canv.addEventListener("touchmove",   handleMove,   false);
 }
 
+// for handling clicks as well as touches
+function onTouch(evt) {
+  evt.preventDefault();
+  if (evt.touches.length > 1 || (evt.type == "touchend" && evt.touches.length > 0))
+    return;
+
+  var newEvt = document.createEvent("MouseEvents");
+  var type = null;
+  var touch = null;
+  switch (evt.type) {
+    case "touchstart":    type = "mousedown";    touch = evt.changedTouches[0];break;
+    case "touchmove":        type = "mousemove";    touch = evt.changedTouches[0];break;
+    case "touchend":        type = "mouseup";    touch = evt.changedTouches[0];break;
+  }
+  
+  newEvt.initMouseEvent(type, true, true, evt.originalTarget.ownerDocument.defaultView, 0,
+    touch.screenX, touch.screenY, touch.clientX, touch.clientY,
+    evt.ctrlKey, evt.altKey, evt.shiftKey, evt.metaKey, 0, null);
+  evt.originalTarget.dispatchEvent(newEvt);
+}
+
 function handleStart(evt) {
     evt.preventDefault();
     var touches = evt.changedTouches;
